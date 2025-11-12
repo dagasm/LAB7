e30f977fdfb251d02a9ff6517667b83dab69a751
dagasm <smintina.ma.david@student.utcluj.ro>
2025-11-12 11:23:31 +0200

    second commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const buton = document.getElementById("buton");
+const nume = document.getElementById("nume");
+const descriere = document.getElementById("descriere");
+
+buton.addEventListener("click",function(){
+	
+	nume.textContent = "Ing. Smintina David-Gabriel";
+	descriere.textContent="DevOps Engineer";
+	
+});
